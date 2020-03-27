d598c584c7af7b47ea2e3f548eafb6f634aac933
Patryk Radziszewski <patryk.radziszewski@instapage.com>
2020-03-27 10:23:55 +0100

    add and configure lerna

--- packages/jest-e2e.config.js
+++ packages/jest-e2e.config.js
@@ -1,19 +1,15 @@
 const tsconfig = require("./tsconfig.json");
-const paths = tsconfig.compilerOptions.paths;
-for (const path in paths) {
-  paths[path][0] = paths[path][0].replace('src/', '');
-}
-tsconfig.compilerOptions.paths = paths;
 const moduleNameMapper = require("tsconfig-paths-jest")(tsconfig);
 
+process.env.DATABASES_POSTGRES_DATABASE = 'todo_e2e';
 module.exports = {
   "moduleFileExtensions": [
     "js",
     "json",
     "ts"
   ],
-  "rootDir": "src",
-  "testRegex": ".spec.ts$",
+  "rootDir": ".",
+  "testRegex": ".e2e-spec.ts$",
   "transform": {
     "^.+\\.(t|j)s$": "ts-jest"
   },
